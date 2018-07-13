59b6b2bfa7a9c5171b3a9e2232394ccf1cc69b80
xiemingsen <674733146@qq.com>
2018-07-13 20:43:49 +0800

    add 查找出现次数超过数组长度一半的数

--- code.cpp
+++ code.cpp
@@ -543,3 +543,38 @@ void FindNumsAppearOnce(vector<int> data,int* num1,int *num2) {
         else
             *num2 ^= data[i];
 }
+
+/*
+数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
+如果不存在则输出0。
+
+在遍历数组时保存两个值：一是数组中一个数字，一是次数。
+遍历下一个数字时，若它与之前保存的数字相同，则次数加1，否则次数减1；
+若次数为0，则保存下一个数字，并将次数置为1。遍历结束后，所保存的数字即为所求。然后再判断它是否符合条件即可。
+*/
+
+int MoreThanHalfNum_Solution(vector<int> numbers) {
+    if (numbers.empty())
+        return 0;
+    int num = numbers[0];
+    int count = 1;
+    for (int i = 1; i < numbers.size(); i++) {
+        if (numbers[i] == num)
+            count++;
+        else
+            count--;
+        if (count == 0) {
+            num = numbers[i];
+            count = 1;
+        }
+    }
+    count = 0;
+    for (int i = 0; i < numbers.size(); i++) {
+        if (num == numbers[i])
+            count++;
+    }
+    if (count > numbers.size() / 2)
+        return num;
+    return 0;
+}
+
