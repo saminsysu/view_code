b2bab253a5421e47d9a6134b6c8ff3bf947e9b60
xiemingsen <674733146@qq.com>
2018-08-04 20:25:52 +0800

    add 第一次只出现一次的字符

--- code.cpp
+++ code.cpp
@@ -902,3 +902,23 @@ ListNode* FindFirstCommonNode(ListNode* pHead1, ListNode* pHead2) {
     }
     return long_list_head;
 }
+
+/* 第一次只出现一次的字符
+在一个字符串(0<=字符串长度<=10000，全部由字母组成)中找到第一个只出现一次的字符，
+并返回它的位置, 如果没有则返回 -1（需要区分大小写）
+使用一个数组统计出现次数
+*/
+int FirstNotRepeatingChar(string str) {
+    if (str.empty())
+        return -1;
+    int chars[256] = {0};
+    for (int i = 0; i < str.size(); i++) {
+        chars[str[i]]++;
+    }
+    for (int i = 0; i < str.size(); i++) {
+        if (chars[str[i]] == 1) {
+            return i;
+        }
+    }
+    return -1;
+}
