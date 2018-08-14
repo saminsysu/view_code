0eddb437df8b37ae1924c74ba140c764995bcc5c
xiemingsen <674733146@qq.com>
2018-08-14 15:32:59 +0800

    栈的压入、弹出序列

--- code.cpp
+++ code.cpp
@@ -1054,3 +1054,28 @@ string ReverseSentence(string str) {
     return r;
 }
 
+/* 栈的压入、弹出序列
+输入两个整数序列，第一个序列表示栈的压入顺序，请判断第二个序列是否可能为该栈的弹出顺序。假设压入栈的所有数字均不相等。
+例如序列1,2,3,4,5是某栈的压入顺序，序列4,5,3,2,1是该压栈序列对应的一个弹出序列，但4,3,5,1,2就不可能是该压栈序列的弹出序列。
+*/
+bool IsPopOrder(vector<int> pushV,vector<int> popV) {
+    int top;
+    int index_of_pop = 0, index_of_push = 0;
+    stack<int> sta;
+    sta.push(pushV[index_of_push++]);
+    while (index_of_pop < popV.size()) {
+        if (popV[index_of_pop] != sta.top() && index_of_push < pushV.size()) {
+            sta.push(pushV[index_of_push++]);
+        } else if (popV[index_of_pop] == sta.top()) {
+            index_of_pop++;
+            sta.pop();
+        } else { // index_of_push >= pushV.size() && popV[index_of_pop] != sta.top()
+            break;
+        }
+    }
+    if (!sta.empty()) {
+        return false;
+    } else {
+        return true;
+    }
+}
