8ee9cc43480ccae9fb307128a2f9fe658ea324ab
xiemingsen <674733146@qq.com>
2018-09-03 18:04:18 +0800

    edit-distance

--- leetcode.cpp
+++ leetcode.cpp
@@ -210,3 +210,41 @@ vector<vector<int> > subsetsWithDup(vector<int> &S) {
     }
     return res;
 }
+
+/* Given two words word1 and word2, find the minimum number of steps required to 
+convert word1 to word2. (each operation is counted as 1 step.)
+
+You have the following 3 operations permitted on a word:
+
+a) Insert a character
+b) Delete a character
+c) Replace a character
+*/
+
+int minDistance(string word1, string word2) {
+    if(word1 == word2) {
+        return 0;
+    }
+    int len1 = word1.size(), len2 = word2.size();
+    if (len1 == 0) {
+        return len2;
+    }
+    if (len2 == 0) {
+        return len1;
+    }
+    int dp[len1+1][len2+1];
+    for (int i = 0; i <= len2; ++i) {
+        dp[0][i] = i;
+    }
+    for (int i = 0; i <= len1; ++i) {
+        dp[i][0] = i;
+    }
+    int k = 0;
+    for (int i = 1; i <= len1; ++i) {
+        for (int j = 1; j <= len2; ++j) {
+            k = (word1[i - 1] == word2[j - 1]) ? 0 : 1; 
+            dp[i][j] = min(min(dp[i - 1][j] + 1, dp[i - 1][j - 1] + k), dp[i][j - 1] + 1);
+        }
+    }
+    return dp[len1][len2];
+}
