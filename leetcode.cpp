f9520df63a8fc4db5b2774725361d13ff9f6a31e
xiemingsen <674733146@qq.com>
2018-09-04 14:56:32 +0800

    add Coin Change

--- leetcode.cpp
+++ leetcode.cpp
@@ -358,3 +358,23 @@ int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
     }
     return dp[m - 1][n - 1];     
 }
+
+/* You are given coins of different denominations and a total amount of money amount. 
+Write a function to compute the fewest number of coins that you need to make up that amount. 
+If that amount of money cannot be made up by any combination of the coins, return -1.
+*/
+
+// 状态转移方程：dp[i] = min(dp[i], dp[i - coins[j]] + 1)
+
+int coinChange(vector<int>& coins, int amount) {
+    vector<int> dp(amount + 1, amount + 1);
+    dp[0] = 0;
+    for (int i = 1; i <= amount; ++i) {
+        for (int j = 0; j < coins.size(); ++j) {
+            if (coins[j] <= i) {
+                dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+            }
+        }
+    }
+    return dp[amount] > amount ? -1 : dp[amount];
+}
