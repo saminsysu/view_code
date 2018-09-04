84db066774d3f50177ff8fa42a33e9a9cae45a97
xiemingsen <674733146@qq.com>
2018-09-04 10:08:50 +0800

    Unique Paths

--- leetcode.cpp
+++ leetcode.cpp
@@ -300,6 +300,33 @@ int maxSubArray(vector<int>& nums) {
 
 The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
 
+How many possible unique paths are there?
+*/
+
+// 状态转移方程：f(i, j) = f(i - 1, j) + f(i, j - 1), i > 0, j > 0
+//                       1, i == 0 || j == 0
+
+int uniquePaths(int m, int n) {
+    if (m <= 0 || n <= 0) {
+        return 0;
+    }
+    int dp[m][n] = {0};
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (i == 0 || j == 0) {
+                dp[i][j] = 1;
+            } else {
+                dp[i][j] = dp[i][j-1] + dp[i-1][j];
+            }
+        }
+    }
+    return dp[m - 1][n - 1];  
+}
+
+/* A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
 Now consider if some obstacles are added to the grids. How many unique paths would there be?
 */
 
